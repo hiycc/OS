77b5dcab4c9933b68420b08e331e87e89d229ef5
Luxi <hiycc@outlook.com>
2020-06-29 23:08:14 +0800

    final version

--- exam_work/client.cpp
+++ exam_work/client.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <iostream>
+#include <fstream>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -22,6 +23,35 @@ int main(){
 
     char bufSend[BUF_SIZE];
     char bufRev[BUF_SIZE];
+    // char buffer[64];
+
+    // ifstream in("out.txt");
+    // if(!in.is_open()){
+    //     cout<<"Read test data error!"<<endl;
+    // }
+    // int i = 0;
+    // while(!in.eof()){
+    //     in.getline(bufSend,64);
+    //     //cout<<buffer;
+    //     //创建套接字
+    //     int sock = socket(AF_INET,SOCK_STREAM,0);   //SOCK_STREAM表示使用面向连接的数据传输方式
+    //     connect(sock,(struct sockaddr*)&serv_addr,sizeof(serv_addr));
+
+    //     string flag = "W:";
+    //     flag.append(std::to_string(i)+","+std::to_string(34)+",");
+    //     char *buf = &flag[0];
+    //     write(sock,buf,flag.size());
+    //     write(sock,bufSend,strlen(bufSend));
+    //     read(sock,bufRev,BUF_SIZE);
+    //     cout<<bufRev<<endl;
+
+    //     memset(bufSend,0,BUF_SIZE);
+    //     memset(bufRev,0,BUF_SIZE);
+
+    //     close(sock);
+    //     i++;
+    // }
+
 
     while(1){
         //创建套接字
@@ -39,38 +69,20 @@ int main(){
         close(sock);
     }
 
+    // for(int i=0;i<1024;i++){
+    //     for(int j=0;j<1024;j++){
+    //         for(int k=0;k<4;k++){
+    //             int sock = socket(AF_INET,SOCK_STREAM,0);   //SOCK_STREAM表示使用面向连接的数据传输方式
+    //             connect(sock,(struct sockaddr*)&serv_addr,sizeof(serv_addr));
+
+    //         }
+    //     }
+    // }
+
     //connect(sock,(struct sockaddr*)&serv_addr,sizeof(serv_addr));
 
 
-    int flag = 1;
 
-    // while(flag){
-    //     //输入0则退出
-    //     cin>>flag;
-    //     if(flag==0){
-    //         cout<<"退出程序"<<endl;
-    //         break;
-    //     }
-
-    //     char *msg;
-    //     cin>>msg;
-    //     write(sock,msg,sizeof(msg));
-        
-    // }
-
-
-    // //char* msgs = "W:4,10,abcdejsdasd";
-    // char str[] = "W:4,13,abcdsdjjklfsa";
-    // write(sock,str,sizeof(str));
-
-    // //读取服务器传回的数据
-    // char buffer[40];
-    // //read(sock,buffer,sizeof(buffer)-1);
-    // write(sock,str,sizeof(str));
-    // //printf("Message from server: %s\n",buffer);
-
-    //关闭套接字
-    // close(sock);
-    // return 0;
+    
 
 }
--- exam_work/server.cpp
+++ exam_work/server.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <cmath>
 #include <stdlib.h>
@@ -14,10 +15,14 @@
 #include <unordered_map>
 using namespace std;
 #define BUF_SIZE 256
+#define MAX_INDEX 4096
+
+//4194304
 
 
 
-const int NUM = 256*1024*1024;
+char *p;
+int Index = 0;
 // //缓存数据对象
 // class InfoNode{
 //     int id; //数据ID
@@ -50,46 +55,83 @@ struct InfoNode{
 class LRUCache{
         
     public:
-        LRUCache(int c):capacity(c){}
+        LRUCache(int c):capacity(c),size(0){}
 
+        int capacity;
+        int size;
         InfoNode* get(int key){
             if(cacheMap.find(key)==cacheMap.end()){
-                InfoNode *p = new InfoNode(-1,0,0,0);
-                return p;
+                InfoNode *q = new InfoNode(-1,0,0,0);
+                return q;
             }else{
                 cachelist.splice(cachelist.begin(),cachelist,cacheMap[key]);
                 cacheMap[key] = cachelist.begin();
                 return cacheMap[key]->getself();
             }
         }
-
+        void display(){
+            cout<<"capacity:"<<capacity<<","<<"size:"<<size<<endl;
+        }
+        
         void set(int id,int size,int index,int num){
             if(cacheMap.find(id)==cacheMap.end()){
-                //淘汰最后一个，然后将其加入到第一个位置
-                if(cachelist.size()==capacity){
+                if(this->size==capacity){
+                    //淘汰最后一个，然后将其加入到第一个位置
+                    char *zero = new char[cachelist.back().size];
+                    memcpy(p+cachelist.back().index*64,zero,cachelist.back().size);
+                    this->size -= cachelist.back().num;
                     cacheMap.erase(cachelist.back().id);
                     cachelist.pop_back();
                 }
                 cachelist.push_front(InfoNode(id,size,index,num));
                 cacheMap[id] = cachelist.begin();
+                this->size+=num;
             }else{
                 //更新节点的值，并将其加到第一个位置
+                size -= cacheMap[id]->num;
                 cacheMap[id]->size = size;
                 cacheMap[id]->id = id;
                 cacheMap[id]->index = index;
                 cacheMap[id]->num = num;
                 cachelist.splice(cachelist.begin(),cachelist,cacheMap[id]);
                 cacheMap[id] = cachelist.begin();
+                this->size+= cacheMap[id]->num;
             }
         }
 
-    private:
-        int capacity;
+        InfoNode* getBack(){
+            return cachelist.back().getself();
+        }
+
+    private:      
         list<InfoNode> cachelist;
         unordered_map<int,list<InfoNode>::iterator> cacheMap;
 
 };
 
+LRUCache *lrucache = new LRUCache(MAX_INDEX);
+
+void ReadTestData(){
+
+    ifstream in("out.txt");
+    if(!in.is_open()){
+        cout<<"Read test data error!"<<endl;
+    }
+    int i = 1;
+    int test_index = 0;
+    char buffer[64];
+    while(!in.eof()){
+        in.getline(buffer,64);
+        cout<<buffer<<endl;
+        lrucache->set(i,34,test_index,1);
+        char *data_pin = &buffer[0];
+        memcpy(p+test_index*64,data_pin,64);
+        i++;
+        test_index++;
+    }
+    in.close();
+    cout<<"Read finish!"<<endl;
+}
 
 
 
@@ -97,13 +139,12 @@ class LRUCache{
 
 
 
-
-int Index = 0;
-LRUCache *lrucache = new LRUCache(NUM);
 
 int main(){
-    char *p = new char[256ULL*1024ULL*1024ULL];
-    mlock(p,256ULL*1024ULL*1024ULL);
+    p = new char[256ULL*1024ULL];
+    mlock(p,256ULL*1024ULL);
+
+    ReadTestData();
 
     //创建套接字
     int serv_sock = socket (AF_INET,SOCK_STREAM,IPPROTO_TCP);
@@ -156,11 +197,27 @@ int main(){
 
             int num = ceil(size/64)+1;
 
+            if((lrucache->size+num)>lrucache->capacity){//全部缓冲区都被占满了
+                //返回<id,"FULL">
+                string bufSend = std::to_string(id);
+                bufSend.append(",FULL");
+                char *bufsend = &bufSend[0];
+                write(clnt_sock,bufsend,strlen(bufsend));
+
+                Index = lrucache->getBack()->index;
+                // InfoNode *back = lrucache->getBack();
+                // int back_index = back->index;
+                // int back_size = back->size;
+                // int back_num = back->num;
+                // char *zero = new char[back_size];
+                // memcpy(p+back_index*64,zero,back_size);
+
+            }
+
             lrucache->set(id,size,Index,num);
             char *data_pin = &data[0];
             memcpy(p+Index*64,data_pin,64);
 
-            Index +=num;
 
             
 
@@ -184,9 +241,9 @@ int main(){
                 write(clnt_sock,bufsend,strlen(bufsend));
             }else{
                 int index = node->index;
-                cout<<index<<endl;
+                //cout<<index<<endl;
                 int size = node->size;
-                cout<<size<<endl;
+                //cout<<size<<endl;
                 char *data = new char[size];
                 strncpy(data,p+index*64,size);
                 //string data(p,index*64,size);
@@ -196,6 +253,8 @@ int main(){
                 char *bufsend = &bufSend[0];
                 write(clnt_sock,bufsend,strlen(bufsend));
             }
+        }else if(buf[0]=='D'){
+            lrucache->display();
         }
 
         
@@ -207,75 +266,6 @@ int main(){
     close(serv_sock);
 
     
-    //接受客户端数据
-    //char buffer[40];
-    //read(clnt_sock,buffer,sizeof(buffer)-1);
-
-    // string buf(buffer);
-    // string b(buf);
-
-    //cout<<buf[0]<<endl;
-    // if(buf[0]=='W'){
-    //     //通过反向寻找第一个“，”，先截取出数据段
-    //     int data_index = buf.rfind(',');
-    //     string data = buf.substr(data_index+1);
-    //     cout<<data<<endl;
-
-    //     //再从buf删去已经截取了数据段的字符串，再通过同样的方法截取出数据id
-    //     buf = buf.substr(0,data_index);
-    //     int size_index = buf.rfind(',');
-    //     string size_str = buf.substr(size_index+1);
-    //     int size = std::stoi(size_str);
-    //     cout<<size<<endl;
-
-    //     buf = buf.substr(0,size_index);
-    //     int id_index = buf.rfind(':');
-    //     string id_str = buf.substr(id_index+1);
-    //     //cout<<"id_str:"<<id_str<<endl;
-    //     int id = std::stoi(id_str);
-    //     cout<<id<<endl;
-
-    //     int num = ceil(size/64);
-
-    //     lrucache->set(id,size,Index,num);
-    //     Index +=num;
-
-    //     char *data_pin = &data[0];
-    //     memcpy(p,data_pin,size);
-
-    //     for(int i=0;i<size;i++){
-    //         cout<<p[i];
-    //     }
-    //     cout<<endl;
-
-
-    //     // char* flag = &b[0];
-    //     // memcpy(p,flag,13);
-
-    //     // char* storebox;
-    //     // memcpy(storebox,p,13);
-    //     // cout<<storebox<<endl;
-        
-        
-    // }
-
-    // delete []p;
-    // //write(clnt_sock,str,sizeof(str));
-    
-    // // printf("Message from client: %s\n",buffer);
-
-    // // while(1){
-    // //     read(clnt_sock,buffer,sizeof(buffer)-1);
-    // //     printf("Message from client: %s\n",buffer);
-    // //     string flag(buffer);
-    // //     if(flag=="exit"){
-    // //         break;
-    // //     }
-    // // }
-
-    // //关闭套接字
-    // close(clnt_sock);
-    // close(serv_sock);
     
 }
 
